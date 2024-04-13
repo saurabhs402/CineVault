d4bfa01b66b1a6c1153133ef5365eb664d50b91b
Saurabh <codefun402@gmail.com>
2024-04-13 14:40:38 +0530

    deployed commit

--- Controllers/homeController.js
+++ Controllers/homeController.js
@@ -3,7 +3,7 @@ const home=function(req,res,next){
 
     res.status(200).json({
         status:"success",
-        message:"API is running successfully.Please enter endpoints to get results.For endpoints you can refer my github Readme\nGithub Repo Link: https://github.com/saurabhs402/CineVault"
+        message:"API is running successfully.Please enter endpoints to get results.For endpoints you can refer my github Readme\nGithub Repo Link: https://github.com/saurabhs402/Cinema-Vault"
     })
 }
 
